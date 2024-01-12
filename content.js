ad0055cb310ec29ff4363c792736ed77bd46d2ca
ameykulkarni03 <112329132+ameykulkarni03@users.noreply.github.com>
2024-01-13 01:32:33 +0530

    Update content.js

--- content.js
+++ content.js
@@ -9,7 +9,7 @@ function analyzeLinks() {
   const links = Array.from(document.getElementsByTagName('a'));
   const linkData = [];
   const baseDomain = new URL(document.baseURI).hostname;
-  const uniqueUrls = new Set(); // Set to store unique URLs
+  const uniqueUrls = new Set();
 
   links.forEach(link => {
       if (!link.href || link.href.trim() === '' || link.href.startsWith('javascript:')) {
@@ -21,10 +21,9 @@ function analyzeLinks() {
       const isNofollow = link.rel.includes('nofollow');
       let type = isNofollow ? 'nofollow' : (isInternal ? 'internal' : 'external');
 
-      // Check if URL is already in the Set
       if (!uniqueUrls.has(link.href)) {
-        uniqueUrls.add(link.href); // Add new URL to the Set
-        linkData.push({ url: link.href, type: type }); // Add link data
+        uniqueUrls.add(link.href);
+        linkData.push({ url: link.href, type: type });
       }
   });
 

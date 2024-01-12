41dd36b83973ef8a212d39c67be4ccc291c848e8
ameykulkarni03 <112329132+ameykulkarni03@users.noreply.github.com>
2024-01-13 01:29:59 +0530

    Update content.js

--- content.js
+++ content.js
@@ -1,32 +1,35 @@
-(function() {  
-    chrome.runtime.onMessage.addListener((message) => {
-      if (message.action === "analyzeLinks") {
-        const linkAnalysis = analyzeLinks();
-        chrome.runtime.sendMessage({ action: "displayLinks", links: linkAnalysis });
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+  if (message.action === "analyzeLinks") {
+      const linkAnalysis = analyzeLinks();
+      chrome.runtime.sendMessage({ action: "displayLinks", links: linkAnalysis });
+  }
+});
+
+function analyzeLinks() {
+  const links = Array.from(document.getElementsByTagName('a'));
+  const linkData = [];
+  const baseDomain = new URL(document.baseURI).hostname;
+  const uniqueUrls = new Set(); // Set to store unique URLs
+
+  links.forEach(link => {
+      if (!link.href || link.href.trim() === '' || link.href.startsWith('javascript:')) {
+          return;
       }
-    });
 
-    function analyzeLinks() {
-        const links = Array.from(document.getElementsByTagName('a'));
-        const linkData = [];
-        const baseDomain = new URL(document.baseURI).hostname;
+      const url = new URL(link.href);
+      const isInternal = url.hostname === baseDomain;
+      const isNofollow = link.rel.includes('nofollow');
+      let type = isNofollow ? 'nofollow' : (isInternal ? 'internal' : 'external');
 
-        links.forEach(link => {
-            if (!link.href || link.href.trim() === '' || link.href.startsWith('javascript:')) {
-                return;
-            }
+      // Check if URL is already in the Set
+      if (!uniqueUrls.has(link.href)) {
+        uniqueUrls.add(link.href); // Add new URL to the Set
+        linkData.push({ url: link.href, type: type }); // Add link data
+      }
+  });
 
-            const url = new URL(link.href);
-            const isInternal = url.hostname === baseDomain;
-            const isNofollow = link.rel.includes('nofollow');
-            let type = isNofollow ? 'nofollow' : (isInternal ? 'internal' : 'external');
+  return linkData;
+}
 
-            linkData.push({ url: link.href, type: type });
-        });
 
-        return linkData;
-    }
 
-    const linkAnalysis = analyzeLinks();
-    chrome.runtime.sendMessage({ action: "displayLinks", links: linkAnalysis });
-})();
